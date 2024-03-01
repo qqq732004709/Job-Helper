b733524ceacf07e8a1f83a47784a11ba4eb3e89b
cxyiscxy <qqq732004709@hotmail.com>
2024-03-01 21:29:59 +0800

    uploaded resume to gpt

--- Job-Helper/Form1.Designer.cs
+++ Job-Helper/Form1.Designer.cs
@@ -28,11 +28,169 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        ChooseResumeDialog = new OpenFileDialog();
+        SelectFile = new Button();
+        FilePath = new Label();
+        SaveKey = new Button();
+        ApiKey = new TextBox();
+        label1 = new Label();
+        label2 = new Label();
+        JobDescription = new TextBox();
+        label3 = new Label();
+        AiResume = new TextBox();
+        label4 = new Label();
+        FetchResume = new Button();
+        CopyResume = new Button();
+        SuspendLayout();
+        // 
+        // ChooseResumeDialog
+        // 
+        ChooseResumeDialog.FileName = "openFileDialog1";
+        ChooseResumeDialog.FileOk += ChooseResumeDialog_FileOk;
+        // 
+        // SelectFile
+        // 
+        SelectFile.Location = new Point(323, 58);
+        SelectFile.Name = "SelectFile";
+        SelectFile.Size = new Size(102, 25);
+        SelectFile.TabIndex = 0;
+        SelectFile.Text = "选择简历";
+        SelectFile.UseVisualStyleBackColor = true;
+        SelectFile.Click += SelectFile_Click;
+        // 
+        // FilePath
+        // 
+        FilePath.AutoSize = true;
+        FilePath.Location = new Point(122, 58);
+        FilePath.Name = "FilePath";
+        FilePath.Size = new Size(0, 17);
+        FilePath.TabIndex = 1;
+        FilePath.Click += FilePath_Click;
+        // 
+        // SaveKey
+        // 
+        SaveKey.Location = new Point(323, 14);
+        SaveKey.Name = "SaveKey";
+        SaveKey.Size = new Size(102, 25);
+        SaveKey.TabIndex = 0;
+        SaveKey.Text = "保存";
+        SaveKey.UseVisualStyleBackColor = true;
+        SaveKey.Click += SaveKey_Click;
+        // 
+        // ApiKey
+        // 
+        ApiKey.Location = new Point(122, 15);
+        ApiKey.Name = "ApiKey";
+        ApiKey.Size = new Size(172, 23);
+        ApiKey.TabIndex = 2;
+        // 
+        // label1
+        // 
+        label1.AutoSize = true;
+        label1.Location = new Point(12, 98);
+        label1.Name = "label1";
+        label1.Size = new Size(104, 17);
+        label1.TabIndex = 3;
+        label1.Text = "请输入岗位需求：";
+        // 
+        // label2
+        // 
+        label2.AutoSize = true;
+        label2.Location = new Point(12, 18);
+        label2.Name = "label2";
+        label2.Size = new Size(100, 17);
+        label2.TabIndex = 3;
+        label2.Text = "Open API Key：";
+        // 
+        // JobDescription
+        // 
+        JobDescription.Location = new Point(122, 98);
+        JobDescription.Multiline = true;
+        JobDescription.Name = "JobDescription";
+        JobDescription.Size = new Size(172, 54);
+        JobDescription.TabIndex = 4;
+        // 
+        // label3
+        // 
+        label3.AutoSize = true;
+        label3.Location = new Point(12, 58);
+        label3.Name = "label3";
+        label3.Size = new Size(71, 17);
+        label3.TabIndex = 1;
+        label3.Text = "请选择文件:";
+        // 
+        // AiResume
+        // 
+        AiResume.Location = new Point(122, 198);
+        AiResume.Multiline = true;
+        AiResume.Name = "AiResume";
+        AiResume.Size = new Size(172, 193);
+        AiResume.TabIndex = 5;
+        // 
+        // label4
+        // 
+        label4.AutoSize = true;
+        label4.Location = new Point(12, 198);
+        label4.Name = "label4";
+        label4.Size = new Size(56, 17);
+        label4.TabIndex = 3;
+        label4.Text = "AI简历：";
+        // 
+        // FetchResume
+        // 
+        FetchResume.Location = new Point(323, 198);
+        FetchResume.Name = "FetchResume";
+        FetchResume.Size = new Size(102, 25);
+        FetchResume.TabIndex = 0;
+        FetchResume.Text = "生成AI简历";
+        FetchResume.UseVisualStyleBackColor = true;
+        FetchResume.Click += FetchResume_Click;
+        // 
+        // CopyResume
+        // 
+        CopyResume.Location = new Point(323, 229);
+        CopyResume.Name = "CopyResume";
+        CopyResume.Size = new Size(102, 25);
+        CopyResume.TabIndex = 0;
+        CopyResume.Text = "复制内容";
+        CopyResume.UseVisualStyleBackColor = true;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(7F, 17F);
+        AutoScaleMode = AutoScaleMode.Font;
+        ClientSize = new Size(446, 403);
+        Controls.Add(AiResume);
+        Controls.Add(JobDescription);
+        Controls.Add(label2);
+        Controls.Add(label4);
+        Controls.Add(label1);
+        Controls.Add(ApiKey);
+        Controls.Add(label3);
+        Controls.Add(FilePath);
+        Controls.Add(SaveKey);
+        Controls.Add(CopyResume);
+        Controls.Add(FetchResume);
+        Controls.Add(SelectFile);
+        Name = "Form1";
+        Text = "工作助手";
+        ResumeLayout(false);
+        PerformLayout();
     }
 
     #endregion
+
+    private OpenFileDialog ChooseResumeDialog;
+    private Button SelectFile;
+    private Label FilePath;
+    private Button SaveKey;
+    private TextBox ApiKey;
+    private Label label1;
+    private Label label2;
+    private TextBox JobDescription;
+    private Label label3;
+    private TextBox AiResume;
+    private Label label4;
+    private Button FetchResume;
+    private Button CopyResume;
 }
--- Job-Helper/Form1.cs
+++ Job-Helper/Form1.cs
@@ -1,9 +1,110 @@
+using OpenAI.Managers;
+using OpenAI;
+using Spire.Pdf;
+using System.Text;
+using OpenAI.ObjectModels.RequestModels;
+using OpenAI.ObjectModels;
+
 namespace Job_Helper;
 
 public partial class Form1 : Form
 {
+    private bool loadedPdf = false;
+    private string pdfContent = string.Empty;
+    private string key = string.Empty;
+
+
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void ChooseResumeDialog_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+
+    }
+
+    private void SelectFile_Click(object sender, EventArgs e)
+    {
+        if (ChooseResumeDialog.ShowDialog() == DialogResult.OK)
+        {
+            try
+            {
+                loadedPdf = false;
+                FilePath.Text = ChooseResumeDialog.FileName;
+
+                PdfDocument document = new();
+                document.LoadFromFile(ChooseResumeDialog.FileName);
+
+                //实例化StringBuilder类，获取文本
+                StringBuilder content = new StringBuilder();
+                content.Append(document.Pages[0].ExtractText());
+                pdfContent = content.ToString();
+
+                loadedPdf = true;
+                MessageBox.Show("PDF内容加载完成");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("遇到问题：e" + ex.Message);
+            }
+        }
+    }
+
+    private void SaveKey_Click(object sender, EventArgs e)
+    {
+        key = ApiKey.Text;
+    }
+
+    private void FilePath_Click(object sender, EventArgs e)
+    {
+
+    }
+
+    private async void FetchResume_Click(object sender, EventArgs e)
+    {
+        if (!loadedPdf
+            || string.IsNullOrWhiteSpace(pdfContent)
+            || string.IsNullOrWhiteSpace(key)
+            || string.IsNullOrEmpty(JobDescription.Text))
+        {
+            MessageBox.Show("请检查是否上传PDF或填写OpenAPI Key以及岗位要求");
+        }
+
+        try
+        {
+            var openAiService = new OpenAIService(new OpenAiOptions()
+            {
+                ApiKey = key
+            });
+
+            var completionResult = await openAiService.ChatCompletion.CreateCompletion(new ChatCompletionCreateRequest
+            {
+                Messages = new List<ChatMessage>
+                {
+                    ChatMessage.FromSystem(@"你是一位精通简历修改与润色的专家。我会提供给你我将应聘的岗位要求与我的简历内容。请你帮我将简历内容进行润色与修改，以下是修改的要求：
+                        1.简历内容要根据我提供的简历内容进行修改
+                        2.简历内容要与目标求职岗位的岗位要求相匹配
+                        3.只返回markdown格式的简历内容，不需要加其他说明文字"),
+                    ChatMessage.FromUser($"我将应聘的岗位要求是：\n\n{JobDescription.Text} \n\n 我的简历内容是：\n\n {pdfContent} \n\n"),
+                },
+                Model = Models.Gpt_3_5_Turbo,
+            });
+
+            if (completionResult.Successful)
+            {
+                AiResume.Text = completionResult.Choices.First().Message.Content;
+            }
+            else
+            {
+                MessageBox.Show($"GPT未成功返回结果：{completionResult.HttpStatusCode}");
+            }
+
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("遇到问题：e" + ex.Message);
+        }
     }
 }
